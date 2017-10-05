8c8a59d99486578ea02293f95aaf932c5b7e221c
Zvonimir Susac <susac.zvonimir@gmail.com>
2017-10-05 12:23:07 +0200

    Refactor network.js script, add readme network example

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ Blockchain class methods *proofOfWork* and *validProof* implements Proof of Work
 # Consensus algorithm
 Consensus algorithm ensures that all nodes in decentralized network reflect the same blockchain. Rule of Consensus algorithm is that the longest valid chain is authoritative.
 
-# Example
+# Full Node Example
 Create a server with REST service to expose blockchain over HTTP protocol. Every instance of this server represents a full node. Full nodes can mine new blocks and receive new transactions. New blocks and transactions are bradcasted to neighboring full nodes. Full nodes use *Consensus algorithm* to replicate distributed ledger (blockchain). 
 
 ```javascript
@@ -389,6 +389,125 @@ $ curl http://127.0.0.1:3000/chain
 
 Every new transaction is added to the local blockchain and broadcasted to the neighbor nodes. Node that first creates a new block, writes down current transactions in that block. You can see how that works by executing previous commands.  
 
+# Peer-To-Peer Network Example
+This script spawns multiple full node servers to mimic real peer-to-peer network, and then executes mining and transaction actions on random nodes.
+At the end, script compares distributed ledgers (blockchains) to test if network reflects the same ledger (blockchain).
+
+```javascript
+const { spawn } = require('child_process')
+const fs = require('fs')
+
+const argv = require('yargs').number('blocks').argv
+const help = require('yargs')
+  .option('blocks', {
+    alias: 'n',
+    describe: 'Number of blocks to mine'
+  })
+  .demandOption(['blocks'], 'Please provide number of blocks mine')
+  .help()
+  .argv
+var blocks = argv.blocks || argv.n
+
+let fullNodes = [
+  {
+    instance: null,
+    name: 'Full Node One',
+    args: ['nodeServer.js', '--port', '3000', '--walletAddress', 'nodeWalletOne', '--neighbors', '127.0.0.1:3001', '127.0.0.1:3002', '127.0.0.1:3003', '127.0.0.1:3004'],
+    mine: '127.0.0.1:3000/mine',
+    transaction: 'http://127.0.0.1:3000/transaction/new'
+  },
+  {
+    instance: null,
+    name: 'Full Node Two',
+    args: ['nodeServer.js', '--port', '3001', '--walletAddress', 'nodeWalletTwo', '--neighbors', '127.0.0.1:3000', '127.0.0.1:3002', '127.0.0.1:3003', '127.0.0.1:3004'],
+    mine: '127.0.0.1:3001/mine',
+    transaction: 'http://127.0.0.1:3001/transaction/new'
+  },
+  {
+    instance: null,
+    name: 'Full Node Three',
+    args: ['nodeServer.js', '--port', '3002', '--walletAddress', 'nodeWalletThree', '--neighbors', '127.0.0.1:3000', '127.0.0.1:3001', '127.0.0.1:3003', '127.0.0.1:3004'],
+    mine: '127.0.0.1:3002/mine',
+    transaction: 'http://127.0.0.1:3002/transaction/new'
+  },
+  {
+    instance: null,
+    name: 'Full Node Four',
+    args: ['nodeServer.js', '--port', '3003', '--walletAddress', 'nodeWalletFour', '--neighbors', '127.0.0.1:3000', '127.0.0.1:3001', '127.0.0.1:3002', '127.0.0.1:3004'],
+    mine: '127.0.0.1:3003/mine',
+    transaction: 'http://127.0.0.1:3003/transaction/new'
+  },
+  {
+    instance: null,
+    name: 'Full Node Five',
+    args: ['nodeServer.js', '--port', '3004', '--walletAddress', 'nodeWalletFive', '--neighbors', '127.0.0.1:3000', '127.0.0.1:3001', '127.0.0.1:3002', '127.0.0.1:3003'],
+    mine: '127.0.0.1:3004/mine',
+    transaction: 'http://127.0.0.1:3004/transaction/new'
+  }
+]
+
+console.log('Initialize Peer-To-Peer network of full nodes \n')
+
+fullNodes.forEach(node => {
+  node.instance = spawn('node', node.args)
+  node.instance.stdout.on('data', (data) => {
+    console.log(`${node.name} stdout: ${data}`)
+  })
+  node.instance.stderr.on('data', (data) => {
+    console.log(`${node.name} stderr: ${data}`)
+  })
+  node.instance.on('close', (code) => {
+    console.log(`${node.name} child process exited with code ${code}`)
+  })
+})
+
+var generateTransaction = () => {
+  return {
+    'sender': Math.random().toString(36).substr(2, 5),
+    'recipient': Math.random().toString(36).substr(2, 5),
+    'value': Math.floor(Math.random() * (1000 - 1 + 1)) + 1
+  }
+}
+
+setTimeout(() => {
+  console.log('Start with network testing \n')
+  const spawn = require('child_process').spawnSync
+  for (var index = 0; index < blocks; index++) {
+    // Make five transactions
+    fullNodes.forEach(node => {
+      // Make new transaction on random node
+      let randomNode = fullNodes[Math.floor(Math.random() * fullNodes.length)]
+      let transaction = spawn('curl', [
+        '-H', 'Content-Type: application/json',
+        '-X', 'POST',
+        '-d', JSON.stringify(generateTransaction()),
+        randomNode.transaction
+      ])
+      console.log(`New Transaction ${randomNode.name} stderr: ${transaction.stderr.toString()}`)
+      console.log(`New Transaction ${randomNode.name} stdout: ${transaction.stdout.toString()}`)
+    })
+
+    // Mine new block on random node
+    let randomNode = fullNodes[Math.floor(Math.random() * fullNodes.length)]
+    let mine = spawn('curl', [ randomNode.mine ])
+    console.log(`Mine ${randomNode.name} stderr: ${mine.stderr.toString()}`)
+    console.log(`Mine ${randomNode.name} stdout: ${mine.stdout.toString()}`)
+  }
+
+  // Compare distributed ledgers (blockchains)
+  let blockchain3000 = JSON.parse(fs.readFileSync('blockchain3000.json', 'utf8'))
+  let blockchain3001 = JSON.parse(fs.readFileSync('blockchain3001.json', 'utf8'))
+  let blockchain3002 = JSON.parse(fs.readFileSync('blockchain3002.json', 'utf8'))
+  let blockchain3003 = JSON.parse(fs.readFileSync('blockchain3003.json', 'utf8'))
+  let blockchain3004 = JSON.parse(fs.readFileSync('blockchain3004.json', 'utf8'))
+  console.log('Peer-to-Peer Network reflects the same blockchain? ',
+    JSON.stringify(blockchain3000) === JSON.stringify(blockchain3001) &&
+    JSON.stringify(blockchain3000) === JSON.stringify(blockchain3002) &&
+    JSON.stringify(blockchain3000) === JSON.stringify(blockchain3003) &&
+    JSON.stringify(blockchain3000) === JSON.stringify(blockchain3004)
+  )
+}, 5000)
+```
 # Tests
 ```
 npm run test
--- example/network.js
+++ example/network.js
@@ -8,161 +8,110 @@
 const { spawn } = require('child_process')
 const fs = require('fs')
 
-// Full node one
-const fullNodeOne = spawn('node', ['nodeServer.js', '--port', '3000', '--walletAddress', 'nodeWalletOne', '--neighbors', '127.0.0.1:3001', '127.0.0.1:3002', '127.0.0.1:3003', '127.0.0.1:3004'])
-fullNodeOne.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`)
-})
-fullNodeOne.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`)
-})
-fullNodeOne.on('close', (code) => {
-  console.log(`child process exited with code ${code}`)
+const argv = require('yargs').number('blocks').argv
+const help = require('yargs')
+  .option('blocks', {
+    alias: 'n',
+    describe: 'Number of blocks to mine'
+  })
+  .demandOption(['blocks'], 'Please provide number of blocks mine')
+  .help()
+  .argv
+var blocks = argv.blocks || argv.n
+
+let fullNodes = [
+  {
+    instance: null,
+    name: 'Full Node One',
+    args: ['nodeServer.js', '--port', '3000', '--walletAddress', 'nodeWalletOne', '--neighbors', '127.0.0.1:3001', '127.0.0.1:3002', '127.0.0.1:3003', '127.0.0.1:3004'],
+    mine: '127.0.0.1:3000/mine',
+    transaction: 'http://127.0.0.1:3000/transaction/new'
+  },
+  {
+    instance: null,
+    name: 'Full Node Two',
+    args: ['nodeServer.js', '--port', '3001', '--walletAddress', 'nodeWalletTwo', '--neighbors', '127.0.0.1:3000', '127.0.0.1:3002', '127.0.0.1:3003', '127.0.0.1:3004'],
+    mine: '127.0.0.1:3001/mine',
+    transaction: 'http://127.0.0.1:3001/transaction/new'
+  },
+  {
+    instance: null,
+    name: 'Full Node Three',
+    args: ['nodeServer.js', '--port', '3002', '--walletAddress', 'nodeWalletThree', '--neighbors', '127.0.0.1:3000', '127.0.0.1:3001', '127.0.0.1:3003', '127.0.0.1:3004'],
+    mine: '127.0.0.1:3002/mine',
+    transaction: 'http://127.0.0.1:3002/transaction/new'
+  },
+  {
+    instance: null,
+    name: 'Full Node Four',
+    args: ['nodeServer.js', '--port', '3003', '--walletAddress', 'nodeWalletFour', '--neighbors', '127.0.0.1:3000', '127.0.0.1:3001', '127.0.0.1:3002', '127.0.0.1:3004'],
+    mine: '127.0.0.1:3003/mine',
+    transaction: 'http://127.0.0.1:3003/transaction/new'
+  },
+  {
+    instance: null,
+    name: 'Full Node Five',
+    args: ['nodeServer.js', '--port', '3004', '--walletAddress', 'nodeWalletFive', '--neighbors', '127.0.0.1:3000', '127.0.0.1:3001', '127.0.0.1:3002', '127.0.0.1:3003'],
+    mine: '127.0.0.1:3004/mine',
+    transaction: 'http://127.0.0.1:3004/transaction/new'
+  }
+]
+
+console.log('Initialize Peer-To-Peer network of full nodes \n')
+
+fullNodes.forEach(node => {
+  node.instance = spawn('node', node.args)
+  node.instance.stdout.on('data', (data) => {
+    console.log(`${node.name} stdout: ${data}`)
+  })
+  node.instance.stderr.on('data', (data) => {
+    console.log(`${node.name} stderr: ${data}`)
+  })
+  node.instance.on('close', (code) => {
+    console.log(`${node.name} child process exited with code ${code}`)
+  })
 })
 
-// Full node two
-const fullNodeTwo = spawn('node', ['nodeServer.js', '--port', '3001', '--walletAddress', 'nodeWalletTwo', '--neighbors', '127.0.0.1:3000', '127.0.0.1:3002', '127.0.0.1:3003', '127.0.0.1:3004'])
-fullNodeTwo.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`)
-})
-fullNodeTwo.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`)
-})
-fullNodeTwo.on('close', (code) => {
-  console.log(`child process exited with code ${code}`)
-})
-
-// Full node three
-const fullNodeThree = spawn('node', ['nodeServer.js', '--port', '3002', '--walletAddress', 'nodeWalletThree', '--neighbors', '127.0.0.1:3000', '127.0.0.1:3001', '127.0.0.1:3003', '127.0.0.1:3004'])
-fullNodeThree.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`)
-})
-fullNodeThree.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`)
-})
-fullNodeThree.on('close', (code) => {
-  console.log(`child process exited with code ${code}`)
-})
-
-// Full node four
-const fullNodeFour = spawn('node', ['nodeServer.js', '--port', '3003', '--walletAddress', 'nodeWalletFour', '--neighbors', '127.0.0.1:3000', '127.0.0.1:3001', '127.0.0.1:3002', '127.0.0.1:3004'])
-fullNodeFour.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`)
-})
-fullNodeFour.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`)
-})
-fullNodeFour.on('close', (code) => {
-  console.log(`child process exited with code ${code}`)
-})
-
-// Full node five
-const fullNodeFive = spawn('node', ['nodeServer.js', '--port', '3004', '--walletAddress', 'nodeWalletFive', '--neighbors', '127.0.0.1:3000', '127.0.0.1:3001', '127.0.0.1:3002', '127.0.0.1:3003'])
-fullNodeFive.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`)
-})
-fullNodeFive.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`)
-})
-fullNodeFive.on('close', (code) => {
-  console.log(`child process exited with code ${code}`)
-})
+var generateTransaction = () => {
+  return {
+    'sender': Math.random().toString(36).substr(2, 5),
+    'recipient': Math.random().toString(36).substr(2, 5),
+    'value': Math.floor(Math.random() * (1000 - 1 + 1)) + 1
+  }
+}
 
 setTimeout(() => {
-  console.log("Let's go!")
+  console.log('Start with network testing \n')
   const spawn = require('child_process').spawnSync
-  for (var index = 0; index < 10; index++) {
-    // Mock mining
-    let mine1 = spawn('curl', [ '127.0.0.1:3000/mine' ])
-    console.log(`stderr: ${mine1.stderr.toString()}`)
-    console.log(`stdout: ${mine1.stdout.toString()}`)
+  for (var index = 0; index < blocks; index++) {
+    // Make five transactions
+    fullNodes.forEach(node => {
+      // Make new transaction on random node
+      let randomNode = fullNodes[Math.floor(Math.random() * fullNodes.length)]
+      let transaction = spawn('curl', [
+        '-H', 'Content-Type: application/json',
+        '-X', 'POST',
+        '-d', JSON.stringify(generateTransaction()),
+        randomNode.transaction
+      ])
+      console.log(`New Transaction ${randomNode.name} stderr: ${transaction.stderr.toString()}`)
+      console.log(`New Transaction ${randomNode.name} stdout: ${transaction.stdout.toString()}`)
+    })
 
-    // Mock transaction
-    let json1 = {
-      'sender': Math.random().toString(36).substr(2, 5),
-      'recipient': Math.random().toString(36).substr(2, 5),
-      'value': Math.floor(Math.random() * (1000 - 1 + 1)) + 1
-    }
-    let transaction1 = spawn('curl', [
-      '-H', 'Content-Type: application/json',
-      '-X', 'POST',
-      '-d', JSON.stringify(json1),
-      'http://127.0.0.1:3001/transaction/new'
-    ])
-    console.log(`stderr: ${transaction1.stderr.toString()}`)
-    console.log(`stdout: ${transaction1.stdout.toString()}`)
-
-    // Mock mining
-    let mine2 = spawn('curl', [ '127.0.0.1:3001/mine' ])
-    console.log(`stderr: ${mine2.stderr.toString()}`)
-    console.log(`stdout: ${mine2.stdout.toString()}`)
-
-    // Mock transaction
-    let json2 = {
-      'sender': Math.random().toString(36).substr(2, 5),
-      'recipient': Math.random().toString(36).substr(2, 5),
-      'value': Math.floor(Math.random() * (1000 - 1 + 1)) + 1
-    }
-    let transaction2 = spawn('curl', [
-      '-H', 'Content-Type: application/json',
-      '-X', 'POST',
-      '-d', JSON.stringify(json2),
-      'http://127.0.0.1:3000/transaction/new'
-    ])
-    console.log(`stderr: ${transaction2.stderr.toString()}`)
-    console.log(`stdout: ${transaction2.stdout.toString()}`)
-
-    // Mock transaction
-    let json3 = {
-      'sender': Math.random().toString(36).substr(2, 5),
-      'recipient': Math.random().toString(36).substr(2, 5),
-      'value': Math.floor(Math.random() * (1000 - 1 + 1)) + 1
-    }
-    let transaction3 = spawn('curl', [
-      '-H', 'Content-Type: application/json',
-      '-X', 'POST',
-      '-d', JSON.stringify(json3),
-      'http://127.0.0.1:3002/transaction/new'
-    ])
-    console.log(`stderr: ${transaction3.stderr.toString()}`)
-    console.log(`stdout: ${transaction3.stdout.toString()}`)
-
-    // Mock transaction
-    let json4 = {
-      'sender': Math.random().toString(36).substr(2, 5),
-      'recipient': Math.random().toString(36).substr(2, 5),
-      'value': Math.floor(Math.random() * (1000 - 1 + 1)) + 1
-    }
-    let transaction4 = spawn('curl', [
-      '-H', 'Content-Type: application/json',
-      '-X', 'POST',
-      '-d', JSON.stringify(json4),
-      'http://127.0.0.1:3002/transaction/new'
-    ])
-    console.log(`stderr: ${transaction4.stderr.toString()}`)
-    console.log(`stdout: ${transaction4.stdout.toString()}`)
-
-    // Mock transaction
-    let json5 = {
-      'sender': Math.random().toString(36).substr(2, 5),
-      'recipient': Math.random().toString(36).substr(2, 5),
-      'value': Math.floor(Math.random() * (1000 - 1 + 1)) + 1
-    }
-    let transaction5 = spawn('curl', [
-      '-H', 'Content-Type: application/json',
-      '-X', 'POST',
-      '-d', JSON.stringify(json5),
-      'http://127.0.0.1:3003/transaction/new'
-    ])
-    console.log(`stderr: ${transaction5.stderr.toString()}`)
-    console.log(`stdout: ${transaction5.stdout.toString()}`)
+    // Mine new block on random node
+    let randomNode = fullNodes[Math.floor(Math.random() * fullNodes.length)]
+    let mine = spawn('curl', [ randomNode.mine ])
+    console.log(`Mine ${randomNode.name} stderr: ${mine.stderr.toString()}`)
+    console.log(`Mine ${randomNode.name} stdout: ${mine.stdout.toString()}`)
   }
+
+  // Compare distributed ledgers (blockchains)
   let blockchain3000 = JSON.parse(fs.readFileSync('blockchain3000.json', 'utf8'))
   let blockchain3001 = JSON.parse(fs.readFileSync('blockchain3001.json', 'utf8'))
   let blockchain3002 = JSON.parse(fs.readFileSync('blockchain3002.json', 'utf8'))
   let blockchain3003 = JSON.parse(fs.readFileSync('blockchain3003.json', 'utf8'))
   let blockchain3004 = JSON.parse(fs.readFileSync('blockchain3004.json', 'utf8'))
-  console.log('Network reflects the same blockchain? ',
+  console.log('Peer-to-Peer Network reflects the same blockchain? ',
     JSON.stringify(blockchain3000) === JSON.stringify(blockchain3001) &&
     JSON.stringify(blockchain3000) === JSON.stringify(blockchain3002) &&
     JSON.stringify(blockchain3000) === JSON.stringify(blockchain3003) &&
